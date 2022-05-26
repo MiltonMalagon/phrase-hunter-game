610759fd621354f9976b8a91bc52b46d00cd3439
MiltonMalagon <miltonmalagon1985@gmail.com>
2022-05-25 19:24:57 -0500

    Added classes

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0; //Number of missed guesses.
+        this.phrases = [
+            new Phrase("Every man is guilty of all the good he did not do"), 
+            new Phrase("The future belongs to those who prepare for it today"), 
+            new Phrase("The only impossible journey is the one you never begin"), 
+            new Phrase("I have no special talent. I am only passionately curious"), 
+            new Phrase("May you live all the days of your life")
+        ]; // Array of phrase objects.
+        this.activePhrase = null; // Phrase object currently in play.
+    }
+
+    startGame() {
+
+    }
+
+    getRandomPhrase() {
+        let random = Math.Math.random() * this.phrases.length;
+    }
+
+    handleInteraction() {
+
+    }
+
+    removeLife() {
+
+    }
+
+    checkForWin() {
+
+    }
+
+    gameOver() {
+        
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game = new Game();
+
+// document.querySelector("#btn__reset").addEventListener("click", e => {
+//     game.startGame();
+//     console.log(e.target)
+// });
+
+// document.addEventListener("click", e => {
+//     if (e.target.className === "key") {
+//         game.handleInteraction();
+//     }
+// });
+
+// const phrase = new Phrase("Life is great when I'm coding!");
+// game.phrases.forEach((phrase, index) => console.log(`Phrase ${index} - phrase: ${phrase.phrase}`));
